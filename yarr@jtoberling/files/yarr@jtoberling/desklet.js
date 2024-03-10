9b8e3cde54b8c462d8d8945c7e0db38ddbaaf837
Tőberling János <jtoberling@localhost.localdomain>
2024-03-10 17:48:07 +0100

    Translation fix

--- yarr@jtoberling/files/yarr@jtoberling/desklet.js
+++ yarr@jtoberling/files/yarr@jtoberling/desklet.js
@@ -18,10 +18,16 @@ const Secret = imports.gi.Secret;
 const Pango = imports.gi.Pango;
 const Clutter = imports.gi.Clutter;
 const fromXML = require('./fromXML');
-
-const uuid = "yarr@jtoberling";
-
 const ByteArray = imports.byteArray;
+
+const UUID = "yarr@jtoberling";
+const DESKLET_ROOT = imports.ui.deskletManager.deskletMeta[UUID].path;
+
+
+function _(str) {
+  return Gettext.dgettext(UUID, str);
+}
+
 
 class YarrDesklet extends Desklet.Desklet {
 
@@ -51,10 +57,16 @@ class YarrDesklet extends Desklet.Desklet {
     clipboard = St.Clipboard.get_default();
 
     constructor (metadata, desklet_id) {
+
+        // translation init: if installed in user context, switch to translations in user's home dir
+        if(!DESKLET_ROOT.startsWith("/usr/share/")) {
+                Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
+        }
+        
         super(metadata, desklet_id);
         this.metadata = metadata;
 
-        this.uuid = this.metadata["uuid"];
+        this.uuid = this.metadata["UUID"];
         
         this.settings = new Settings.DeskletSettings(this, this.metadata.uuid, this.instance_id);
 
@@ -625,7 +637,7 @@ class YarrDesklet extends Desklet.Desklet {
     displayItems(context) {
     
         let updated= new Date();
-        context.headTitle.set_text(_('Updated:') + context._formatedDate(new Date()));
+        context.headTitle.set_text(_('Updated') + ': ' + context._formatedDate(new Date()));
     
         context.tableContainer.destroy_all_children();
         
